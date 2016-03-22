292e412191dbfb2c66b4079fdda056e0447ef08e
Ryan Wilkins <wilkinsrs@outlook.com>
2016-03-22 19:26:42 -0400

    add mailing list sign-up functionality

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,23 @@
 class VisitorsController < ApplicationController
 
   def new
-    @owner = Owner.new
+    @visitor = Visitor.new
   end
 
+  def create
+    if @visitor = Visitor.new(secure_params)
+      @visitor.subscribe
+      flash[:notice] = "Signed up #{@visitor.email}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def secure_params
+      params.require(:visitor).permit(:email)
+    end
+  
 end
