3a0f149d08f8e89ffb6fb135c67d36230e003542
Ryan Wilkins <wilkinsrs@outlook.com>
2016-03-24 07:43:17 -0400

    email set for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,18 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
+  # email enabled in production
+ config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.perform_deliveries = true
+ config.action_mailer.raise_delivery_errors = false
+ config.action_mailer.smtp_settings = {
+   address: "smtp.mandrillapp.com",
+   port: 587,
+   user_name: Rails.application.secrets.email_provider_username,
+   password: Rails.application.secrets.email_provider_api_key
+}
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
